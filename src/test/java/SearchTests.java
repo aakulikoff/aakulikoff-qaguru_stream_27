b706a4c6002e764f507ed54fd7898eb0d857c4bb
Andrey Kulikov <9230697@gmail.com>
2024-12-23 14:37:57 +0300

    дз 1-4

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,14 +1,32 @@
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
     @Test
-    void successfulSearchTest() {
+    void successfulGoogleSelenideSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+//        $("#main").shouldHave(text("https://ru.selenide.org"));
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+
+    @Test
+    void successfulGoogleMosSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("mos ru").pressEnter();
+//        $("#main").shouldHave(text("https://ru.selenide.org"));
+        $("[id=search]").shouldHave(text("Официальный сайт Мэра Москвы"));
+    }
+
+    @Test
+    void successfulYandexMosSearchTest() {
+        open("https://ya.ru/");
+        $(".search3__input").click();
+        $("[id=search-result]").shouldHave(text("Официальный сайт Мэра Москвы"));
     }
 }
+
