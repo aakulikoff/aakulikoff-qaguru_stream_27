2c71bff31af5a1066486f180e6eec17e446f1066
Andrey Kulikov <9230697@gmail.com>
2025-03-28 11:53:18 +0300

    Добавил модель lombok

--- 27.8/src/test/java/tests/StatusExtendedTests.java
+++ 27.8/src/test/java/tests/StatusExtendedTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import models.lombok.*;
+import models.pojo.*;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,7 +12,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StatusExtendedTests extends TestBase {
 
     @Test
-    void successfulLogin2() {
+    void successfulLoginPojo() {
+
+        LoginBodyModels authData = new LoginBodyModels();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponseModels response = given()
+                .contentType(JSON)
+                .body(authData)
+        .when()
+                .post("login")
+        .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseModels.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+    @Test
+    void successfulLoginLombok() {
 
         LoginBodyLombokModels authData = new LoginBodyLombokModels();
         authData.setEmail("eve.holt@reqres.in");
@@ -20,9 +42,9 @@ public class StatusExtendedTests extends TestBase {
         LoginResponseLombokModels response = given()
                 .contentType(JSON)
                 .body(authData)
-        .when()
+                .when()
                 .post("login")
-        .then()
+                .then()
                 .log().status()
                 .log().body()
                 .statusCode(200)
