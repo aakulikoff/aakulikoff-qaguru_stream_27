2f8b99898d0560aab254539db622dbdacf6535e7
Andrey Kulikov <9230697@gmail.com>
2025-03-28 12:45:01 +0300

    Добавил тесты с шагами аллюр

--- 27.8/src/test/java/tests/StatusExtendedTests.java
+++ 27.8/src/test/java/tests/StatusExtendedTests.java
@@ -1,9 +1,11 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import models.lombok.*;
 import models.pojo.*;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
@@ -12,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StatusExtendedTests extends TestBase {
 
     @Test
-    void successfulLoginPojo() {
+    void successfulLoginPojoTest() {
 
         LoginBodyModels authData = new LoginBodyModels();
         authData.setEmail("eve.holt@reqres.in");
@@ -21,6 +23,9 @@ public class StatusExtendedTests extends TestBase {
         LoginResponseModels response = given()
                 .contentType(JSON)
                 .body(authData)
+                .log().uri()
+                .log().body()
+                .log().headers()
         .when()
                 .post("login")
         .then()
@@ -33,13 +38,16 @@ public class StatusExtendedTests extends TestBase {
     }
 
     @Test
-    void successfulLoginLombok() {
+    void successfulLoginLombokTest() {
 
         LoginBodyLombokModels authData = new LoginBodyLombokModels();
         authData.setEmail("eve.holt@reqres.in");
         authData.setPassword("cityslicka");
 
         LoginResponseLombokModels response = given()
+                .log().uri()
+                .log().body()
+                .log().headers()
                 .contentType(JSON)
                 .body(authData)
                 .when()
@@ -51,6 +59,83 @@ public class StatusExtendedTests extends TestBase {
                 .extract().as(LoginResponseLombokModels.class);
 
         assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+    @Test
+    void successfulLoginAllureTest() {
+
+        LoginBodyLombokModels authData = new LoginBodyLombokModels();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponseLombokModels response = given()
+                .filter(new AllureRestAssured())
+                .log().uri()
+                .log().body()
+                .log().headers()
+                .contentType(JSON)
+                .body(authData)
+        .when()
+                .post("login")
+        .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModels.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+    @Test
+    void successfulLoginCustomAllureTest() {
+
+        LoginBodyLombokModels authData = new LoginBodyLombokModels();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponseLombokModels response = given()
+                .filter(new AllureRestAssured())
+                .log().uri()
+                .log().body()
+                .log().headers()
+                .contentType(JSON)
+                .body(authData)
+        .when()
+                .post("login")
+        .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModels.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+    @Test
+    void successfulLoginAllureStepsTest() {
+
+        LoginBodyLombokModels authData = new LoginBodyLombokModels();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponseLombokModels response = step( "Сделать запрос", ()->
+                     given()
+                            .filter(new AllureRestAssured())
+                            .log().uri()
+                            .log().body()
+                            .log().headers()
+                            .contentType(JSON)
+                            .body(authData)
+                    .when()
+                            .post("login")
+                    .then()
+                            .log().status()
+                            .log().body()
+                            .statusCode(200)
+                            .extract().as(LoginResponseLombokModels.class));
+
+        step( "Проверить ответ", ()->
+            assertEquals("QpwL5tke4Pnpja7X4", response.getToken()));
     }
 
 }
