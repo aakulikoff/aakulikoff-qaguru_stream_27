7ef65cc9d228cbc8d3fb4ecf0eb17f1b487a96b5
Andrey Kulikov <9230697@gmail.com>
2025-08-25 17:27:07 +0300

    Добавил дз 11. Attach

--- 27.11_DemoQa#4&Jenkins/src/test/java/tests/TestBase.java
+++ 27.11_DemoQa#4&Jenkins/src/test/java/tests/TestBase.java
@@ -2,6 +2,8 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
